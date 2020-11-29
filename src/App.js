64adc1b4490ad704767db605a9c772d2ccda0ae0
smarten <smarten.developer@gmail.com>
2020-11-28 22:10:56 -0800

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Contact from "./components/Contact";
 
 function App() {
   return (
-    <Router basename={"https://sean-marten.github.io/react-portfolio-3/"}>
+    <Router basename={""}>
       <Navbar />
       <Route path="/contact" component={Contact} />
       <Route path="/portfolio" component={Portfolio} />
